981f615261e6cc03e9271afae08c0c636493689c
Михаил Акмурзин <akmurzinmihail@gmail.com>
2024-10-11 12:10:39 +0500

    Ориентация северÐ я хочу чтобы ты верил я зхочу чÐтобы ты плакалÐ

--- labs/lab1/misha/result.py
+++ labs/lab1/misha/result.py
@@ -4,17 +4,22 @@ import pandas as pd
 # Чтение данных
 data = pd.read_csv('output.txt', sep=' ')
 
-# Определяем количество данных для одного участка
 end = 1660
-
-# Убираем лишние точки из цикла
-for i in range(4):
-    start = i * end
-    end = (i + 1) * end
-    plt.plot(data.x2[start:end], data.y2[start:end], label=f"Марс, участок {i + 1}")
-
-plt.xlabel('x')
-plt.ylabel('y')
+# plt.plot(data.x1,data.y1)
+plt.plot(data.x2[:end],data.y2[:end],label='Марс')
+plt.plot(data.x3[:end],data.y3[:end],label='Спутник')
 plt.legend()
-plt.grid(True)
 plt.show()
+
+
+
+# for i in range(4):
+#     start = i * end
+#     end = (i + 1) * end
+#     plt.plot(data.x3[start:end], data.y3[start:end], label=f"Марс, участок {i + 1}")
+#
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.legend()
+# plt.grid(True)
+# plt.show()
