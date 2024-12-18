d59552ca36d272c66173f0a4b688e3b01ad6dfbf
misha <akmurzinmihail@gmail.com>
2024-12-18 18:59:28 +0500

    - такое чувство что то мешает обновляться значениям.Надо разобраться.

--- labs/lab1/misha/src/task2.cpp
+++ labs/lab1/misha/src/task2.cpp
@@ -26,7 +26,7 @@ struct Constants {
     static constexpr double H = 200e3; // высота орбиты (м)
     static constexpr double M0 = 10.0; // масса полезной назрузки (кг)
     static constexpr double U = 3040.0; // скорость истечения (м/c)
-    static constexpr double koef = 0.05;
+    static constexpr double koef = 0.025;
 };
 
 // Вспомогательные функции
@@ -47,7 +47,7 @@ public:
         if (t >= Constants::T) {
             return Constants::M0;
         } else {
-            return (Constants::M0 + mt) / (1 - Constants::koef) - mt * t / Constants::T;
+            return (Constants::M0 + mt) / (1 - Constants::koef) - (mt * t) / Constants::T;
         }
     }
 
@@ -64,7 +64,7 @@ public:
         double rx = y[0], ry = y[1], vx = y[4], vy = y[5]; // ракета
         double r13x = y[2], r13y = y[3], v3x = y[6], v3y = y[7]; // cпутник
 
-        if (sqrt((rx - r13x) * (rx - r13x) + (ry - r13y) * (ry - r13y)) < Constants::R2) {
+        if (sqrt((rx - r13x) * (rx - r13x) + (ry - r13y) * (ry - r13y)) <= Constants::R3) {
             throw std::runtime_error("Ракета попала в планету.");
         }
         // Расчёт расстояний
@@ -73,7 +73,6 @@ public:
         double r3 = distance(rx, ry, r13x, r13y); // расстояние от ракеты до спутника
         double r13 = std::sqrt(r13x * r13x + r13y * r13y); // расстояние от спутника до солнца
         double r23 = distance(r13x, r13y, r12x, r12y); // расстояние от спутника до планеты
-
 
         // Расчет скорости
         double v = std::sqrt(vx * vx + vy * vy); // скорость ракеты
@@ -88,16 +87,16 @@ public:
         // Ускорения для ракеты
         f[4] = -Constants::U * dm(t) / m(t) * vx / v +
                Constants::G * (
-                   -Constants::M1 * rx / std::pow(r, 3) -
-                   Constants::M2 * (rx - r12x) / std::pow(r2, 3) -
-                   Constants::M3 * (rx - r13x) / std::pow(r3, 3)
+                   -Constants::M1 * rx / std::pow(r, 3)
+                   - Constants::M2 * (rx - r12x) / std::pow(r2, 3)
+                   - Constants::M3 * (rx - r13x) / std::pow(r3, 3)
                );
 
         f[5] = -Constants::U * dm(t) / m(t) * vy / v +
                Constants::G * (
-                   -Constants::M1 * ry / std::pow(r, 3) -
-                   Constants::M2 * (ry - r12y) / std::pow(r2, 3) -
-                   Constants::M3 * (ry - r13y) / std::pow(r3, 3)
+                   -Constants::M1 * ry / std::pow(r, 3)
+                   - Constants::M2 * (ry - r12y) / std::pow(r2, 3)
+                   - Constants::M3 * (ry - r13y) / std::pow(r3, 3)
                );
 
         // Ускорения для спутника
@@ -107,9 +106,9 @@ public:
         f[7] = -Constants::G * Constants::M1 * r13y / std::pow(r13, 3) -
                Constants::G * Constants::M2 * (r13y - r12y) / std::pow(r23, 3);
 
-        double force_sun_x = -Constants::M1 * rx / pow(r, 3);
-        double force_planet_x = -Constants::M2 * (rx - r12x) / pow(r2, 3);
-        double force_satellite_x = -Constants::M3 * (rx - r13x) / pow(r3, 3);
+        // double force_sun_x = -Constants::M1 * rx / pow(r, 3);
+        // double force_planet_x = -Constants::M2 * (rx - r12x) / pow(r2, 3);
+        // double force_satellite_x = -Constants::M3 * (rx - r13x) / pow(r3, 3);
         // cout << "Forces X: " << force_sun_x << " " << force_planet_x << " " << force_satellite_x << endl;
     }
 };
@@ -121,9 +120,9 @@ double Physics::r12y;
 int main() {
     double r12x0 = -50368219856.43, r12y0 = -219503615669.65,
             r13x0 = -50370393498.29, r13y0 = -219513089385.80,
-            v3x0 = 25513.15, v3y0 = -6666.22; // нач координаты планеты и спутника
-    Physics::mt = 1000;
-    double angle = 270. * M_PI / 180;
+            v3x0 = 25513.15, v3y0 = 6666.22; // нач координаты планеты и спутника
+    Physics::mt = 10000;
+    double angle = 90. * M_PI / 180;
 
     double rx0, ry0, vx0, vy0;
 
@@ -153,24 +152,28 @@ int main() {
     };
 
     double t = 0.0;
-    double t_circle_end = 60. * 60* 200 ;
+    double t_circle_end = 60. * 60 * 20;
     double t_end = t_circle_end;
     double h = 0.1;
 
     double t_curr = t;
-    runge_kutta_cash_karp54<state_type> stepper;
+    runge_kutta_dopri5<state_type> stepper;
 
-
-    try {
-        while (t_curr < t_end) {
-            stepper.do_step(Physics::calculateForces, y, t, h);
-            fout_main << y[0] << " " << y[1] << " " << y[2] << " " << y[3] << endl;
-            t_curr += h;
-        }
-        fout_main.close();
-    } catch (exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
+    integrate_adaptive(stepper, Physics::calculateForces, y, t, t_end, h,
+                       [&](const state_type &state, double t) {
+                           fout_main << y[0] << " " << y[1] << " " << y[2] << " " << y[3] << endl;
+                       });
+    fout_main.close();
+    // try {
+    //     while (t_curr < t_end) {
+    //         stepper.do_step(Physics::calculateForces, y, t, h);
+    //
+    //         t_curr += h;
+    //     }
+    //     fout_main.close();
+    // } catch (exception &e) {
+    //     std::cerr << e.what() << std::endl;
+    // }
 
     return 0;
 }
