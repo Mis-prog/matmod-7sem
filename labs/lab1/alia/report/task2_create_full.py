dbbb93cf04fa71cd3329d4c222a09b7f79b29006
Alia <aliagalyamowa@yandex.ru>
2025-01-31 16:36:25 +0500

    Update plot optimal fuel

--- labs/lab1/alia/report/task2_create_full.py
+++ labs/lab1/alia/report/task2_create_full.py
@@ -2,43 +2,11 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-# # Путь к файлу
-# file_path = "full_fuel.txt"
-#
-# # Регулярное выражение
-# pattern = r"mt\s*=\s*([\d.]+)\s*для\s*угла\s*(\d+)"
-#
-#
-# MT=[]
-# ANGLE=[]
-# # Открываем файл и считываем построчно
-# with open(file_path, "r", encoding="utf-8") as file:
-#     for line in file:
-#         # Ищем совпадения в текущей строке
-#         match = re.search(pattern, line)
-#         if match:
-#             mt, angle = match.groups()
-#             MT.append(float(mt))
-#             ANGLE.append(float(angle))
-#
-# plt.plot(ANGLE, MT)
-# plt.xlabel("Угол")
-# plt.ylabel("Кол-во топлива")
-# plt.grid()
-# plt.show()
+# Путь к файлу
+file_path = "full_fuel.txt"
 
-values = []
-file = open('fuel_optimize.txt', 'r')
-while True:
-    content = file.readline()
-    if not content:
-        break
-    value = tuple(map(float,content.split()))
-    values.append(value)
-file.close()
-
-x_values = [value[0] for value in values]
-y_values = [value[1] for value in values]
+# Регулярное выражение
+pattern = r"mt\s*=\s*([\d.]+)\s*для\s*угла\s*(\d+)"
 
 
 MT=[]
@@ -53,13 +21,22 @@ with open(file_path, "r", encoding="utf-8") as file:
             MT.append(float(mt))
             ANGLE.append(float(angle))
 
-MT=np.array(MT)
-i=MT.argmin()
-
-plt.scatter(ANGLE[i],MT[i],label=f'Минимальное кол-во топлива {MT[i]} для угла {ANGLE[i]}')
 plt.plot(ANGLE, MT)
 plt.xlabel("Угол")
 plt.ylabel("Кол-во топлива")
 plt.grid()
-plt.legend()
 plt.show()
+
+values = []
+file = open('fuel_optimize.txt', 'r')
+while True:
+    content = file.readline()
+    if not content:
+        break
+    value = tuple(map(float,content.split()))
+    values.append(value)
+file.close()
+
+x_values = [value[0] for value in values]
+y_values = [value[1] for value in values]
+
